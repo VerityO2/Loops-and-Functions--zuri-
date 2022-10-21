ff4733b4224dd21740da50770781bc3338c7b983
VerityO2 <114458775+VerityO2@users.noreply.github.com>
2022-10-21 18:01:16 +0100

    final touch

--- userAuth/php/login.php
+++ userAuth/php/login.php
@@ -9,28 +9,24 @@ if (isset($_POST['submit'])) {
 function loginUser($username, $password)
 {
     $dir = "..\storage\users.csv";
-    $fopn = fopen($dir, 'r');
-    $fget = fgets($fopn);
+    $usercontent = file($dir);
 
-    $usercontent = file($fget);
 
     foreach ($usercontent as $index => $value) {
-        do {
-            if ($username === $value) {
-                if (!$password  === $value) {
-                    header('Location: ..\forms\login.html?error');
-                    exit();
-                } else {
-                    session_start();
-                    $_SESSION['username'] = $_POST['email'];
-                    header('Location: ..\dashboard.php?success');
-                    exit();
-                }
-            } else {
+
+        if ($password = $value) {
+            if (!$username  = $value) {
                 header('Location: ..\forms\login.html?error');
                 exit();
+            } else {
+                session_start();
+                $_SESSION['Username'] = $_POST['email'];
+                header('Location: ..\dashboard.php?success');
+                exit();
             }
-        } while ($index > 0);
+        } else {
+            header('Location: ..\forms\login.html?error');
+            exit();
+        }
     }
-    fclose($fopn);
 }
